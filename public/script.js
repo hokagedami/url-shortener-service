f198422c608b0b9a77960e7e53bc8842063bf22b
Michael Akinyemi <Michael.Akinyemi@interswitchgroup.com>
2022-02-02 12:35:01 +0100

    Adding copy to clipboard feature

--- public/script.js
+++ public/script.js
@@ -10,3 +10,16 @@ const disableResultView = () => {
         result.style.display = 'none'
     }
 }
+
+async function copyToClipboard() {
+    /* Get the text field */
+    const copyText = document.getElementById("shortenurl");
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+    /* Copy the text inside the text field */
+    await navigator.clipboard.writeText(copyText.value)
+    const balloon = document.getElementById("balloon");
+    balloon.style.display = 'block'
+}
